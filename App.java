5726a8187a8b412ca5b5254982283001792c3a34
Diego Alejandro Ferrada Veliz <131547632+WinstonProgrammer@users.noreply.github.com>
2023-09-20 18:32:28 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,7 @@
+package paquetin;
+
+public class App extends Postfija {
+    public static void main(String[] args) throws Exception {
+        System.out.println(convertirAPostfija("1-2-4^5*3*6/7^2^2"));
+    }
+}
